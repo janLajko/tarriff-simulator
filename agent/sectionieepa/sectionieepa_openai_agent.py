bb9b82714d18e7fe20f4b97a37e9eba0edf1976a
janLajko <janlajko1@gmail.com>
2025-11-27 21:50:43 +0800

    update

--- agent/sectionieepa/sectionieepa_openai_agent.py
+++ agent/sectionieepa/sectionieepa_openai_agent.py
@@ -48,8 +48,13 @@ BULK_QUERY_CHUNK = 50
 T = TypeVar("T")
 
 DEFAULT_HEADINGS = [
-    # "9903.01.77"
-    "9903.01.84"
+    "9903.02.02",
+    "9903.01.10",
+    "9903.01.24",
+    "9903.01.25",
+    "9903.01.24",
+    "9903.01.77",
+    "9903.01.84",
 ]
 
 LLM_MEASURE_PROMPT = """You are a legal text structure analyzer for HTSUS Section ieepa derivative steel measures.
